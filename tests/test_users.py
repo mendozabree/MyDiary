826ca013ff606b250b52ebf15831c9f98447b9e8
Bridget Mendoza <mendozabridget777@gmail.com>
2018-07-28 16:32:51 +0300

    [#159311281] Create failing test for login

--- tests/test_users.py
+++ tests/test_users.py
@@ -6,8 +6,8 @@ from api import app
 
 class UserTests(unittest.TestCase):
 
+    @staticmethod
     def create_app():
-
         app['TESTING'] = True
 
     def setUp(self):
@@ -24,12 +24,19 @@ class UserTests(unittest.TestCase):
 
     def test_API_can_signup_user(self):
         test_user = app.test_client(self)
-
         response = test_user.post('/api/v1/auth/signup',
                                   data=json.dumps(self.user[0]),
                                   content_type='application/json')
         self.assertEqual(response.status_code, 201)
         self.assertIn('Success', str(response.data))
+
+    def test_API_can_login_user(self):
+        test_user = app.test_client(self)
+        response = test_user.post('/api/v1/auth/login',
+                                  data=json.dumps(self.user[0]),
+                                  content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Welcome', str(response.data))
 
 
 if __name__ == '__main__':
