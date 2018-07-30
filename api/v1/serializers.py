b8cd3d6fcc808c594963a11b0a001793c234480e
Bridget Mendoza <mendozabridget777@gmail.com>
2018-07-30 15:16:02 +0300

    Create output and input models

--- api/v1/serializers.py
+++ api/v1/serializers.py
@@ -6,11 +6,23 @@ from api import api
 user_creation_model = api.model('Create_User', {
     'username': fields.String('Your user name', description='username'),
     'first_name': fields.String('Your first name', description='User first name'),
-    'second_name': fields.String('Your last name', description='User last name'),
+    'last_name': fields.String('Your last name', description='User last name'),
     'email': fields.String('Your email', description='User email'),
     'password': fields.String('Your password', description='User password')
 })
 login_model = api.model('Login_User', {
     'username': fields.String('Your user name', description='username'),
     'password': fields.String('Your password', description='User password')
+})
+
+entry_creation_model = api.model('Create_Entry', {
+    'title': fields.String('Your title', description='Entry title'),
+    'content': fields.String('Your content', description='Entry content')
+})
+
+entry_get_model = api.model('Get_all_Entries', {
+    'title': fields.String('Your title', description='Entry title'),
+    'content': fields.String('Your content', description='Entry content'),
+    'entry_date': fields.String('Your entry date', description='Entry date'),
+    'entry_time': fields.String('Your entry time', description='Entry time')
 })
