d5afc29d746d09375734090c0dc4dc13191bccd5
Bridget Mendoza <mendozabridget777@gmail.com>
2018-07-30 15:14:26 +0300

    Create the entries endpoint

--- api/v1/endpoints/entries.py
+++ api/v1/endpoints/entries.py
@@ -8,13 +8,13 @@ from flask_jwt_extended import get_jwt_identity, jwt_required
 
 from api import api
 from api.v1.database import DatabaseConnection
-from api.v1.serializers import entry_creation_model
+from api.v1.serializers import entry_creation_model, entry_get_model
 from ..classes import Entries
 
 
 @api.route('/api/v1/entries')
 class NewEntry(Resource):
-
+    """Class for making a new entry"""
     @api.expect(entry_creation_model)
     @jwt_required
     def post(self):
@@ -23,10 +23,43 @@ class NewEntry(Resource):
 
         new_entry = Entries(title=new_entry_data['title'],
                             content=new_entry_data['content'],
-                            entry_date=new_entry_data['entry_date'],
-                            entry_time=new_entry_data['entry_time'],
-                            author=current_user
+                            user_id=current_user
                             )
         new_entry.create_entry()
 
         return 'Success', 201
+
+
+@api.route('/api/v1/entries')
+class RetrieveAll(Resource):
+    @jwt_required
+    def get(self):
+        current_user = get_jwt_identity()
+
+        my_entries = Entries.retrieve_all_entries(user=current_user)
+
+        return my_entries, 200
+
+
+@api.route('/api/v1/entries/<int:entry_id>')
+class GetSpecificEntry(Resource):
+    @jwt_required
+    def get(self, entry_id):
+        output = Entries.get_specific_entry(entry_id=entry_id)
+        if output:
+            return output, 200
+        else:
+            return 'No entry found, check id', 400
+
+
+@api.route('/api/v1/entries/<int:entry_id>')
+class ModifyEntry(Resource):
+    @jwt_required
+    def put(self, entry_id):
+        modify_data = api.payload
+        current_user = get_jwt_identity()
+
+        response = Entries.modify_entry(entry_id=entry_id, modify_data=
+                                        modify_data, user=current_user)
+
+        return response
