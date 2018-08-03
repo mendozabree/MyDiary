ebb8e941bd2f1da2e91d67312f5e1df90400d60f
Bridget Mendoza <mendozabridget777@gmail.com>
2018-08-03 06:16:31 +0300

    Clean up code

--- api/v1/endpoints/entries.py
+++ api/v1/endpoints/entries.py
@@ -47,22 +47,6 @@ class RetrieveAll(Resource):
 
 
 @api.route('/api/v1/entries/<int:entry_id>')
-class GetSpecificEntry(Resource):
-    """Class for retrieving specific entry"""
-    @jwt_required
-    def get(self, entry_id):
-        """Method to get specific entry"""
-        current_user = get_jwt_identity()
-        output = entry.get_specific(entry_id=entry_id,
-                                    current_user=current_user)
-        if output:
-
-            return {'message': output}, 200
-        else:
-            return {'message': 'No entry found, check id'}, 404
-
-
-@api.route('/api/v1/entries/<int:entry_id>')
 class ModifyEntry(Resource):
     """Class to modify all entries"""
 
