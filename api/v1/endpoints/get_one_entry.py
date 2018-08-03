8f82b80144b067b7d15e616663c36c3e3a6283b1
Bridget Mendoza <mendozabridget777@gmail.com>
2018-08-03 06:13:03 +0300

    Move endpoint to new file

---
+++ api/v1/endpoints/get_one_entry.py
@@ -0,0 +1,24 @@
+from flask_restplus import Resource
+from flask_jwt_extended import get_jwt_identity, jwt_required
+
+from api import api
+from api.v1.serializers import entry_creation_model, specific_entry
+from api.v1.database import Entry
+
+entry = Entry()
+
+
+@api.route('/api/v1/entries/<int:entry_id>')
+class GetSpecificEntry(Resource):
+    """Class for retrieving specific entry"""
+    @jwt_required
+    def get(self, entry_id):
+        """Method to get specific entry"""
+        current_user = get_jwt_identity()
+        output = entry.get_specific(entry_id=entry_id,
+                                    current_user=current_user)
+        if output:
+
+            return {'message': output}, 200
+        else:
+            return {'message': 'No entry found, check id'}, 404
