da32f2536998dbe50da6bf8678096c05c7b84db4
Bridget Mendoza <mendozabridget777@gmail.com>
2018-08-12 07:39:04 +0300

    Create register user functionality

--- UI/static/diary.js
+++ UI/static/diary.js
@@ -1,3 +1,32 @@
 function pickDate(){
     document.getElementById('my_date').setAttribute("type", "date");
 }
+function registerUser() {
+    let firstname = document.getElementById('firstName').value;
+    let lastname = document.getElementById('lastName').value;
+    let username = document.getElementById('userName').value;
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+
+    fetch('http://127.0.0.1:5000/api/v1/auth/signup', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json, text/plain, */*' ,
+            'Content-Type': 'application/json; charset=UTF-8'
+        },
+        body: JSON.stringify({
+            username: username, first_name: firstname, last_name: lastname,
+            email: email, password: password
+        })
+    })
+        .then((response) => response.json())
+        .then(function (data) {
+            let output = data['message']['message']
+            if (output === 'User successfully registered.'){
+                location.href='./home.html'
+            }else{
+                document.getElementById('output').innerHTML = data['message']['message'];
+                console.log('Request succeeded with JSON response', data)
+            }
+        })
+}
--- api/__init__.py
+++ api/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_restplus import Api
 from flask_jwt_extended import JWTManager
-
+from flask_cors import CORS
 
 app = Flask(__name__)
 api = Api(app)
+CORS(app)
 
 secret_key = 'thisismysecretkey'
 app.config['JWT_SECRET_KEY'] = secret_key
