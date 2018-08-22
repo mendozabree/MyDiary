01274eee928b618f1f69eecdc0bfa23b874a25d8
mendozabree <mendozabridget777@gmail.com>
2018-08-22 19:23:50 +0300

    Add view and modify entry

---
+++ UI/static/newEntry.js
@@ -0,0 +1,20 @@
+function userEntry() {
+    let title = document.getElementById('my_title').value;
+    let content = document.getElementById('my_content').value;
+    fetch('http://127.0.0.1:5000/api/v1/entries', {
+        method: 'POST',
+        headers: {Authorization : `Bearer ${document.cookie}`,
+        'Content-Type':'application/json'},
+        body: JSON.stringify({title:title, content:content})
+    })
+        .then((response) => response.json())
+        .then(function (data) {
+			if (data['message']['status'] === 'Success') {
+                console.log(data['message']['message'])
+                location.href='home.html'
+
+			}else{
+				alert(data['message']['msg'])
+			}
+        })
+}
